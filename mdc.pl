df3570c8cdc0c6ba1d9cffdeba596ae7aef769d5
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-09-02 19:01:51 +0200

    Add a way to in/decrement volume level

--- mdc.pl
+++ mdc.pl
@@ -10,16 +10,50 @@ set 'show_errors' 	=> 1;
 set 'logger' 		=> 'console';
 set 'log' 			=> 'debug';
 
-sub send_command {
-	my ($command) = @_;
+sub get_conn {
 	my $conn = new Net::Telnet;
 	$conn->open(
 		Host => "localhost",
 		Port => 6600
 	);
+	return $conn;
+}
+
+sub send_command {
+	my ($command) = @_;
+	my $conn = get_conn;
 	$conn->print($command);
 	my $result = $conn->getline;
 	$conn->close;
+	return $result;
+}
+
+sub change_volume {
+	my ($command) = @_;
+
+	my $conn = get_conn;
+	$conn->print('status');
+
+	# first line is useless
+	$conn->getline;
+
+	# second contain current volume
+	my $current_volume = $conn->getline;
+	# empty the buffer
+	$conn->buffer_empty;
+
+	# Increment the volume
+	$current_volume =~ s/volume:\s(\d+)/$1/;
+	$current_volume += 3 if $command eq "+";
+	$current_volume -= 3 if $command eq "-";
+
+	# correct if $current_volume > 100 or < 0
+	$current_volume = 0 if $current_volume < 0;
+	$current_volume = 100 if $current_volume > 100;
+
+	my $command = "setvol ".$current_volume;
+	$conn->print($command);
+	my $result = $conn->getline;
 	return $result;
 }
 
@@ -31,5 +65,8 @@ get "/stop"		=> sub { return send_command('stop');	  };
 # prev/next
 get "/previous"	=> sub { return send_command('previous'); };
 get "/next"		=> sub { return send_command('next');	  };
+
+get "/volinc" 	=> sub { return change_volume('+'); };
+get "/voldec" 	=> sub { return change_volume('-'); };
 
 dance;
